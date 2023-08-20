4b1c7c9ca76ff4d2a1f447fede9640ef5015aeed
dopnetindo <dopnetindo@gmail.com>
2023-08-20 10:22:54 +0700

    Pendahuluan OOP

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('Hello world!')
+"""
+Python Object Oriented Programming
+
+1. Define Class
+2. Define Atribute
+3. Define Method
+4. Define Instance / Turunan
+
+"""
+
+
+# Cara membuat class
+class Hero:
+    pass
+
+
+# Instance / Turunan Object
+hero1 = Hero()
+hero2 = Hero()
+
+# Atribut
+hero1.name = "Gani"
+hero1.health = 100
+
+hero2.name = "Shiska"
+hero2.health = 101
+
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
