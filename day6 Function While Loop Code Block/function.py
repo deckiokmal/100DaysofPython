2beea274b04d8cf0fbbd700a0a5e3bf6c41127ca
dopnetindo <dopnetindo@gmail.com>
2023-09-17 12:44:39 +0700

    day6

---
+++ day6 Function While Loop Code Block/function.py
@@ -0,0 +1,12 @@
+# create our own function instead builtin function
+# def keyword to create function
+
+
+# create a function
+def my_function():
+    print("hello")
+    print("bye")
+
+
+# calling the function
+my_function()
